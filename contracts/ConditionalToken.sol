36a75ee9d1548333ed4be07472831d82ba4b018f
SamWarden <SamWardenSad@gmail.com>
2021-03-25 16:18:14 +0300

    Add decimals to ConditionalToken

--- contracts/ConditionalToken.sol
+++ contracts/ConditionalToken.sol
@@ -6,9 +6,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ConditionalToken is ERC20, Ownable {
+    uint8 private _decimals;
 
-    constructor() public {
-        _owner = msg.sender;
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_)
+        public
+    {
+        _decimals = decimals_;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
     }
 
     function mint(address account, uint256 amount)
