64c037cd31a5e73b2c9777fd6bcebf1bc6c07acd
SamWarden <SamWardenSad@gmail.com>
2021-03-26 21:16:18 +0300

    Add statuses to the market

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -4,16 +4,24 @@ pragma solidity ^0.6.0;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "./balancer/BPool.sol";
 import "./ConditionalToken.sol";
 
-contract Market is BPool {
+contract Market is BPool, Ownable {
+    enum Status {Created, Open, Closed}
+
+    Status status = Status.Created;
 
     constructor() public {}
 
-    function cloneConstructor() public {
+    function cloneConstructor () public onlyOwner {
+        require(status == status.Created, "Status has to be Created");
 
+        status = Status.Open
+    }
+
+    function getStatus() public view returns (Status) {
+        return status;
     }
 }
