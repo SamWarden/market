b45c64b0f4783125aca7806ae17249b4e0ecd256
SamWarden <SamWardenSad@gmail.com>
2021-03-27 21:07:01 +0300

    Patch some vars in chainlink

--- contracts/Chainlink.sol
+++ contracts/Chainlink.sol
@@ -1,48 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-import "./balancer/BPool.sol";
 import "./balancer/BFactory.sol";
 import "./ConditionalToken.sol";
 
 contract MarketFactory is Ownable {
-    using SafeMath for uint256, uint8;
+    // using SafeMath for uint256, uint8;
 
     mapping(string => address) public feeds;
-    string[] public feedsPairs;
+
+    AggregatorV3Interface internal priceFeed;
+    string[] public feedPairs;
 
     function setFeed(
-        string _keyPair,
+        string _currencyPair,
         address _chainlinkFeed
     ) public onlyOwner {
-        //Save the _keyPair to feedsPais if it isn't there and add the feed with the pair
-        if (feeds[_keyPair] == address(0)) {
-            feedsPairs.push(_keyPair)
+        //TODO: or allow set address(0) and delete the pair from feedPairs
+        require(_chainlinkFeed != address(0), "Address of chainlink feed cannot be 0")
+        //Save the _currencyPair to feedPairs if it isn't there and add the feed with the pair
+        if (feeds[_currencyPair] == address(0)) {
+            feedPairs.push(_currencyPair)
         }
-        feeds[_keyPair] = _chainlinkFeed; 
+        feeds[_currencyPair] = _chainlinkFeed; 
     }
 
     /**
      * Returns the latest price
      */
-    function getLatestPrice(AggregatorV3Interface feed)
+    function getLatestPrice(AggregatorV3Interface _feed)
         public
         view
         returns (int256)
     {
         (
-            uint80 roundID,
-            int256 price,
-            uint256 startedAt,
-            uint256 timeStamp,
-            uint80 answeredInRound
-        ) = feed.latestRoundData();
-        return price;
+            uint80 _roundID,
+            int256 _price,
+            uint256 _startedAt,
+            uint256 _timeStamp,
+            uint80 _answeredInRound
+        ) = _feed.latestRoundData();
+        return _price;
     }
 
     /**
@@ -56,20 +58,20 @@ contract MarketFactory is Ownable {
      *
      * @dev A timestamp with zero value means the round is not complete and should not be used.
      */
-    function getHistoricalPrice(AggregatorV3Interface feed, uint80 roundId)
+    function getHistoricalPrice(AggregatorV3Interface _feed, uint80 _roundId)
         public
         view
         returns (int256)
     {
         (
-            uint80 id,
-            int256 price,
-            uint256 startedAt,
-            uint256 timeStamp,
-            uint80 answeredInRound
-        ) = feed.getRoundData(roundId);
-        require(timeStamp > 0, "Round not complete");
-        return price;
+            uint80 _id,
+            int256 _price,
+            uint256 _startedAt,
+            uint256 _timeStamp,
+            uint80 _answeredInRound
+        ) = _feed.getRoundData(_roundId);
+        require(_timeStamp > 0, "Round not complete");
+        return _price;
     }
 
 }
