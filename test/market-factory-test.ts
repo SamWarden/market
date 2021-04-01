5567136bdf49c3cf7a33fb975f969a17f8cd672c
SamWarden <SamWardenSad@gmail.com>
2021-04-01 05:51:36 +0000

    Add market-factpry-test

---
+++ test/market-factory-test.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+import { Signer, ContractFactory, Contract } from "ethers";
+import { expect } from "chai";
+
+describe("MarketFactory", async () => {
+  console.log("start");
+  let accounts: Signer[];
+
+  beforeEach(async () => {
+    accounts = await ethers.getSigners();
+  });
+
+  it("test_deploy", async () => {
+    const MarketFactory: ContractFactory = await ethers.getContractFactory("MarketFactory");
+    const marketFactory: Contract = await MarketFactory.deploy();
+
+    console.log(marketFactory.address);
+    console.log(marketFactory.deployTransaction.hash);
+
+    await marketFactory.deplyed();
+
+    console.log(await marketFactory.getStage());
+    expect(await marketFactory.getStage()).to.equal("Created");
+  });
+});
