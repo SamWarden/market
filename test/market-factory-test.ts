7e75ee534a46a652719e75fb279f9b8e4013297e
SamWarden <SamWardenSad@gmail.com>
2021-04-03 19:37:36 +0300

    Add comments to market-factory-test

--- test/market-factory-test.ts
+++ test/market-factory-test.ts
@@ -8,27 +8,46 @@ describe("MarketFactory", async () => {
   let accounts: SignerWithAddress[];
 
   beforeEach(async () => {
+    // Get list of signers
     accounts = await ethers.getSigners();
   });
 
   it("test_deploy", async () => {
+    // Get factories of contracts
     const MarketFactory: ContractFactory = await ethers.getContractFactory("MarketFactory");
+    const Market: ContractFactory = await ethers.getContractFactory("Market");
     const TToken: ContractFactory = await ethers.getContractFactory("TToken");
-    const signer: SignerWithAddress = accounts[0];
-    const collateralBalance = "100000";
 
+    // Get account what will call methods
+    const signer: SignerWithAddress = accounts[0];
+
+    // Set balance of collateral token for initial liquidity and freezed tokens
+    const collateralBalance: string = "100000";
+    // Set the duration of a market
+    const duration: number = 10000;
+
+    // Deploy DAI contract
     const dai: Contract = await TToken.deploy("DAI", "DAI", 18);
     await dai.deployed();
+    // Mint tokens to the account
     await dai.mint(signer.address, ethers.utils.parseEther(collateralBalance));
 
+    // Deploy the MarketFactory contract
     const marketFactory: Contract = await MarketFactory.deploy(dai.address);
-
-    console.log(marketFactory.address);
-    console.log(marketFactory.deployTransaction.hash);
-
     await marketFactory.deployed();
+    // Load contract of baseMarket
+    const market: Contract = Market.attach(await marketFactory.baseMarket());
+    // Listen events from LOG_CALL
+    market.on(await market.filters.LOG_CALL(), (res) => {console.log(res)});
 
-    //console.log(await marketFactory.getStage());
-    //expect(await marketFactory.getStage()).to.equal("Created");
+    // Log addresses
+    console.log("Signer_address: " + signer.address);
+    console.log("MarketFactory_address: " + marketFactory.address);
+    console.log("MarketFactory_hash: " + marketFactory.deployTransaction.hash);
+    console.log("Market_address: " + market.address);
+    console.log("Market_owner: " + await market.owner());
+
+    // Create market
+    await marketFactory.create("DAI", "ETH/USD", duration, collateralBalance);
   });
 });
