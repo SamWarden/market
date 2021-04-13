f9e31ae5f5654d79c2c72fbcddb0aa6f9ace42a8
SamWarden <SamWardenSad@gmail.com>
2021-04-13 03:38:53 +0300

    Add tests in market-factory

--- test/market-factory-test.ts
+++ test/market-factory-test.ts
@@ -3,11 +3,12 @@ import { Signer, Contract, BigNumber } from "ethers";
 import chai from "chai";
 // import chaiAsPromised from "chai-as-promised";
 import { solidity } from "ethereum-waffle";
+import { AddressZero } from "@ethersproject/constants";
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import type { TransactionResponse, TransactionReceipt, Log } from "@ethersproject/abstract-provider";
 import type { TransactionReceiptWithEvents } from "../src/types";
-import type {
-  MarketFactory__factory, MarketFactory as MarketFactoryCreate,
+import {
+  MarketFactory__factory, MarketFactory as MarketFactoryContract,
   TClones__factory, TClones as TClonesContract,
   Market__factory, Market as MarketContract,
   TToken__factory, TToken as TTokenContract,
@@ -17,7 +18,6 @@ import type {
 // chai.use(chaiAsPromised);
 chai.use(solidity);
 const { expect } = chai;
-
 
 describe("MarketFactory", async () => {
   console.log("start");
@@ -32,17 +32,27 @@ describe("MarketFactory", async () => {
   let baseMarket: MarketContract;
   let baseConditionalToken: ConditionalTokenContract;
   const collateralBalance: BigNumber = ethers.utils.parseEther("100000");
-  const duration: number = 10000;
+  const conditionalBalance: BigNumber = collateralBalance.div(2);
+  const duration: number = 1;
   const collateralName: string = "DAI";
   const currencyPair: string = "ETH/USD";
-  const protocolFee: BigNumber = ethers.utils.parseEther("0.3");
+  const protocolFee: BigNumber = ethers.utils.parseEther("1").div(1000).mul(3);
+  const swapFee: BigNumber = ethers.utils.parseEther("1").div(1000).mul(3);
 
   before(async () => {
+    accounts = await ethers.getSigners();
+    owner = accounts[0];
+    account = accounts[1];
+
     // Get factories of contracts
     MarketFactory = await ethers.getContractFactory("MarketFactory") as MarketFactory__factory;
     Market = await ethers.getContractFactory("Market") as Market__factory;
     TToken = await ethers.getContractFactory("TToken") as TToken__factory;
     ConditionalToken = await ethers.getContractFactory("ConditionalToken") as ConditionalToken__factory;
+    // MarketFactory = new MarketFactory__factory(owner);
+    // Market = new Market__factory(owner);
+    // TToken = new TToken__factory(owner);
+    // ConditionalToken = new ConditionalToken__factory(owner);
 
     baseMarket = await Market.deploy();
     await baseMarket.deployed();
@@ -54,22 +64,17 @@ describe("MarketFactory", async () => {
     // Deploy DAI contract
     dai = await TToken.deploy("DAI", "DAI", 18);
     await dai.deployed();
-
-    // Get list of signers
-    accounts = await ethers.getSigners();
-    owner = accounts[0];
-    account = accounts[1];
   });
 
   it("test_deploy", async () => {
     // Deploy the MarketFactory contract
-    const marketFactory: MarketFactoryCreate = await MarketFactory.deploy(baseMarket.address, baseConditionalToken.address, dai.address);
+    const marketFactory: MarketFactoryContract = await MarketFactory.deploy(baseMarket.address, baseConditionalToken.address, dai.address);
     await marketFactory.deployed();
   });
 
   describe("deployed", async () => {
-    let marketFactory: MarketFactoryCreate;
-    let accMarketFactory: MarketFactoryCreate;
+    let marketFactory: MarketFactoryContract;
+    let accMarketFactory: MarketFactoryContract;
 
     beforeEach(async () => {
       // Deploy the MarketFactory contract
@@ -83,37 +88,23 @@ describe("MarketFactory", async () => {
       await dai.mint(owner.address, collateralBalance);
       // Approve collateral balance to the factory
       await dai.approve(marketFactory.address, collateralBalance);
-      console.log(await dai.allowance(owner.address, marketFactory.address), await dai.allowance(owner.address, marketFactory.address) == collateralBalance);
       // Create market
-      expect(await marketFactory.create(collateralName, currencyPair, duration, collateralBalance)).to.emit(marketFactory, "Created");
+      const tx = await marketFactory.create(collateralName, currencyPair, duration, collateralBalance);
+      expect(tx).to.emit(marketFactory, "Created");
+      const txReceipt = await tx.wait();
+      const market = Market.attach(txReceipt.events![txReceipt.events!.length - 1].args![0]);
+
+      const bull = ConditionalToken.attach(await market.bullToken());
+      const bear = ConditionalToken.attach(await market.bearToken());
+
+      expect(await market.balanceOf(owner.address)).to.equal(await market.INIT_POOL_SUPPLY());
+      expect(await bull.balanceOf(owner.address)).to.equal(conditionalBalance);
+      expect(await bear.balanceOf(owner.address)).to.equal(conditionalBalance);
+
       // await expect(accMarketFactory.create(collateralName, currencyPair, duration, collateralBalance)).to.be.revertedWith("Ownable: caller is not the owner");
       await expect(marketFactory.create("None", currencyPair, duration, collateralBalance)).to.be.revertedWith("Invalid colleteral currency");
       await expect(marketFactory.create(collateralName, "None/None", duration, collateralBalance)).to.be.revertedWith("Invalid currency pair");
-      await expect(marketFactory.create(collateralName, currencyPair, 1, collateralBalance)).to.be.revertedWith("Invalid duration");
-    });
-
-    describe("create_market", async () => {
-      let market: MarketContract;
-
-      beforeEach(async () => {
-        // Mint tokens to the account
-        await dai.mint(owner.address, collateralBalance);
-        // Approve collateral balance to the factory
-        await dai.approve(marketFactory.address, collateralBalance);
-        console.log(await dai.allowance(owner.address, marketFactory.address), await dai.allowance(owner.address, marketFactory.address) == collateralBalance);
-        // Create market
-        const tx = await marketFactory.create(collateralName, currencyPair, duration, collateralBalance);
-
-        expect(tx).to.emit(marketFactory, "Created");
-        //TransactionReceiptWithEvents
-        const txReceipt = await tx.wait();
-        market = Market.attach(txReceipt.events![txReceipt.events!.length - 1].args![0]);
-        console.log(await market.bull());
-      });
-
-      it("test_market_check", async () => {
-        expect(await marketFactory.isMarket(market.address)).to.be.true;
-      });
+      // await expect(marketFactory.create(collateralName, currencyPair, 1, collateralBalance)).to.be.revertedWith("Invalid duration");
     });
         // .withArgs(address, "ETH/USD", "DAI", now, duration);
       // let txReceipt: TransactionReceiptWithEvents = await tx.wait();
@@ -134,7 +125,6 @@ describe("MarketFactory", async () => {
     });
 
     it("test_swap_fee", async () => {
-      const swapFee: BigNumber = ethers.utils.parseEther("1").div(1000).mul(3);
       // expect(await marketFactory.swapFee()).to.equal(ethers.utils.parseEther("0.3"));
       expect(await marketFactory.setSwapFee(swapFee));
       expect(await marketFactory.swapFee()).to.equal(swapFee);
@@ -148,6 +138,74 @@ describe("MarketFactory", async () => {
       expect(await marketFactory.collect(dai.address));
       expect(await dai.balanceOf(owner.address)).to.equal(collectAmount);
       await expect(accMarketFactory.collect(dai.address)).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    describe("create_market", async () => {
+      let market: MarketContract;
+      let bull: ConditionalTokenContract;
+      let bear: ConditionalTokenContract;
+
+      beforeEach(async () => {
+        // Mint tokens to the account
+        await dai.mint(owner.address, collateralBalance);
+        // Approve collateral balance to the factory
+        await dai.approve(marketFactory.address, collateralBalance);
+
+        await marketFactory.setSwapFee(swapFee);
+        // Create market
+        const tx = await marketFactory.create(collateralName, currencyPair, duration, collateralBalance);
+        //TransactionReceiptWithEvents
+        const txReceipt = await tx.wait();
+        market = Market.attach(txReceipt.events![txReceipt.events!.length - 1].args![0]);
+        console.log(await market.bullToken());
+
+        bull = ConditionalToken.attach(await market.bullToken());
+        bear = ConditionalToken.attach(await market.bearToken());
+
+        // Mint tokens to the account
+        await dai.mint(owner.address, collateralBalance);
+        // Approve collateral balance to the market
+        await dai.approve(market.address, collateralBalance);
+      });
+
+      it("test_market_check", async () => {
+        expect(await marketFactory.isMarket(market.address)).to.be.true;
+        expect(await marketFactory.isMarket(AddressZero)).to.be.false;
+      });
+
+      it("test_market_buy", async () => {
+        expect(await bull.balanceOf(owner.address)).to.equal(conditionalBalance);
+        expect(await market.buy(conditionalBalance)).to.emit(market, "Buy");
+        expect(await bull.balanceOf(owner.address)).to.equal(conditionalBalance.mul(2));
+      });
+
+      it("test_market_close", async () => {
+        expect(await market.close(2)).to.emit(market, "Closed");
+        await expect(market.buy(conditionalBalance)).to.be.revertedWith("Market: this market is not open");
+      });
+
+      it("test_market_redeem", async () => {
+        await expect(market.redeem(conditionalBalance)).to.be.revertedWith("Market: this market is not closed");
+        expect(await market.close(2)).to.emit(market, "Closed");
+        await bull.approve(market.address, conditionalBalance);
+        expect(await market.redeem(conditionalBalance)).to.emit(market, "Redeem");
+        expect(await bull.balanceOf(owner.address)).to.equal(0);
+        expect(await bear.balanceOf(owner.address)).to.equal(conditionalBalance);
+        expect(await dai.balanceOf(owner.address)).to.equal(collateralBalance.add(conditionalBalance));
+      });
+
+      it("test_market_spot_price", async () => {
+        expect(await market.getSpotPrice(dai.address, bull.address)).to.equal(BigNumber.from("501504513540621866"));
+      });
+
+      it("test_market_spot_price_sans_fee", async () => {
+        expect(await market.getSpotPriceSansFee(dai.address, bull.address)).to.equal(ethers.utils.parseEther("0.5"));
+      });
+
+      it("test_market_join_pool", async () => {
+        console.log(await market.totalSupply());
+        expect(await market.joinPool(10, [1, 1, 1]));
+      });
     });
   });
 });
