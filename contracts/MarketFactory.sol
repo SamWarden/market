0ac9ec6329a9c58eeb888e713003dc9561b22d09
SamWarden <SamWardenSad@gmail.com>
2021-04-14 12:02:04 +0300

    Move logic from Market to MarketFactory

--- contracts/MarketFactory.sol
+++ contracts/MarketFactory.sol
@@ -29,8 +29,16 @@ contract MarketFactory is Ownable, ChainlinkData {
     );
     // event NewConditionalToken(address indexed contractAddress, uint256 _time);
 
+    struct MarketStruct {
+        bool exist;
+        // uint marketID;
+        uint totalDeposit;
+        uint totalRedemption;
+        address market;
+    }
+
     //TODO: add list of markets and currencies
-    mapping(address => bool) public markets;
+    mapping(address => MarketStruct) public markets;
     mapping(string => address) public colleteralCurrencies;
 
     //Variables
@@ -42,9 +50,9 @@ contract MarketFactory is Ownable, ChainlinkData {
     address private baseConditionalToken;
     uint public protocolFee;
     uint public swapFee;
+    address private factory;
 
     //Constants
-    // uint256 public constant CONDITIONAL_TOKEN_WEIGHT = (10).mul(BConst.BONE);
     uint256 public constant CONDITIONAL_TOKEN_WEIGHT = 10 * 10**18;
     uint256 public constant COLLATERAL_TOKEN_WEIGHT  = CONDITIONAL_TOKEN_WEIGHT * 2;
 
@@ -55,6 +63,7 @@ contract MarketFactory is Ownable, ChainlinkData {
         colleteralCurrencies["DAI"] = _collateralToken; //0x9326BFA02ADD2366b30bacB125260Af641031331; //!WRONG ADDRESS
 
         swapFee = Market(_baseMarket).MIN_FEE();
+        factory = msg.sender;
     }
 
     function create(
@@ -83,11 +92,11 @@ contract MarketFactory is Ownable, ChainlinkData {
         ERC20 _collateralToken = ERC20(colleteralCurrencies[_collateralCurrency]);
         uint8 _collateralDecimals = _collateralToken.decimals();
 
-        //Pull collateral tokens from sender
-        _collateralToken.transferFrom(msg.sender, address(this), _approvedBalance);
-
         //Estamate balance tokens
         uint256 _initialBalance = SafeMath.div(_approvedBalance, 2);
+
+        //Pull collateral tokens from sender
+        _collateralToken.transferFrom(msg.sender, address(this), _initialBalance);
 
         //Contract factory (clone) for two ERC20 tokens
         ConditionalToken _bullToken = cloneConditionalToken("Bull", "Bull", _collateralDecimals);
@@ -114,30 +123,150 @@ contract MarketFactory is Ownable, ChainlinkData {
         addToken(_marketAddress, _collateralToken, _initialBalance, COLLATERAL_TOKEN_WEIGHT);
         // addCollateralToken(_marketAddress, _collateralToken, _initialBalance);
 
+        //Finalize the pool, get initial LP tokens and allow public swaps
+        _market.finalize();
+
+        MarketStruct memory marketStruct =
+            MarketStruct({
+                exist: true,
+                totalDeposit: 0,
+                totalRedemption: 0,
+                market: _marketAddress
+            });
+
+        //Save the market
+        markets[_marketAddress] = marketStruct;
+        marketList.push(_marketAddress);
+
         //Approve pool to buy tokens
         _collateralToken.approve(_marketAddress, _initialBalance);
 
         //Mint the conditional tokens
-        _market.buy(_initialBalance);
-
-        //Finalize the pool, get initial LP tokens and allow public swaps
-        _market.finalize();
+        buy(_marketAddress, _initialBalance);
 
         //Send LP and bought conditional tokens to the sender
         _market.transfer(msg.sender, _market.INIT_POOL_SUPPLY());
-        _bullToken.transfer(msg.sender, _initialBalance);
-        _bearToken.transfer(msg.sender, _initialBalance);
+        // _bullToken.transfer(msg.sender, _initialBalance);
+        // _bearToken.transfer(msg.sender, _initialBalance);
 
-        markets[_marketAddress] = true;
-        marketList.push(_marketAddress);
-
-        emit Created(_marketAddress, _feedCurrencyPair, _collateralCurrency, now, _duration);
+        // emit Created(_marketAddress, _feedCurrencyPair, _collateralCurrency, now, _duration);
 
         return _marketAddress;
     }
 
+    function close(address _marketAddress)
+        external
+        // _logs_
+        // _lock_
+    {
+        Market _market = Market(_marketAddress);
+        require(markets[_marketAddress].exist, "Market doesn't exist");
+        require(_market.stage() == Market.Stages.Open, "MarketFactory: this market is not open");
+        // now > created + duration
+        require(
+            now > SafeMath.add(
+                _market.created(),
+                _market.duration()
+            ),
+            "Market closing time hasn't yet arrived"
+        );
+        //Get chainlink price feed by _baseCurrencyID
+        // address _chainlinkPriceFeed =
+        //     baseCurrencyToChainlinkFeed[markets[_market].baseCurrencyID];
+
+        //TODO: Query chainlink
+        int256 price = 1;
+        int256 _finalPrice = price;
+
+        //TODO: maybe should move it to the method?
+        // require(finalPrice > 0, "Chainlink error");
+        //TODO: require(initialPrice != _finalPrice, "Price didn't change");
+
+        require(_finalPrice > 0, "Chainlink error");
+        // require(markets[_market].initialPrice != _finalPrice, "Price didn't change");
+        
+        _market.close(_finalPrice);
+        
+        // emit Closed(_market, now, finalPrice, result, winningToken);
+    }
+
+    //Buy new token pair for collateral token
+    function buy(address _marketAddress, uint _amount)
+        public
+        // _logs_
+        // _lock_
+    {
+        Market _market = Market(_marketAddress);
+        require(markets[_marketAddress].exist, "Market doesn't exist");
+        require(_market.stage() == Market.Stages.Open, "MarketFactory: this market is not open");
+        require(_amount > 0, "Invalid amount");
+
+        //Deposit collateral
+        require(ERC20(_market.collateralToken()).transferFrom(msg.sender, address(this), _amount));
+
+        //Mint both tokens for user
+        ConditionalToken(_market.bearToken()).mint(msg.sender, _amount);
+        ConditionalToken(_market.bullToken()).mint(msg.sender, _amount);
+
+        //Increase total deposited collateral
+        markets[_marketAddress].totalDeposit = SafeMath.add(
+            markets[_marketAddress].totalDeposit,
+            _amount
+        );
+
+        //TODO: addd the _market arg
+        // emit Buy(_market, msg.sender, _amount, now);
+    }
+
+    function redeem(address _marketAddress, uint _amount)
+        external
+        // _logs_
+        // _lock_
+    {
+        Market _market = Market(_marketAddress);
+        require(markets[_marketAddress].exist, "Market doesn't exist");
+        require(_market.stage() == Market.Stages.Closed, "MarketFactory: this market is not closed");
+        require(_amount > 0, "Invalid amount");
+        require(markets[_marketAddress].totalDeposit >= SafeMath.add(markets[_marketAddress].totalRedemption, _amount), "No collateral left");
+
+        if (_market.result() != Market.Results.Draw) {
+            //If there is winner
+            //Burn win tokens from a sender
+            ConditionalToken(_market.winningToken()).burnFrom(msg.sender, _amount);
+        } else {
+            // if a Draw
+            // address -= addressAllowance / ((bearAllow + bullAllow) / (amount * 2))
+            // Get allowance of conditional tokens from the sender
+            ConditionalToken _bullToken = ConditionalToken(_market.bullToken());
+            ConditionalToken _bearToken = ConditionalToken(_market.bearToken());
+            uint256 _bullAllowance = _bullToken.allowance(msg.sender, address(this));
+            uint256 _bearAllowance = _bearToken.allowance(msg.sender, address(this));
+            require(SafeMath.add(_bullAllowance, _bearAllowance) < SafeMath.mul(_amount, 2), "Total allowance of conditonal tokens is lower than the given amount");
+            // ratio = totalAllowance / conditionalAmount
+            uint256 ratio = SafeMath.div(SafeMath.add(_bullAllowance, _bearAllowance), SafeMath.mul(_amount, 2));
+
+            // if not 0, burn the tokens
+            if (_bullAllowance > 0) {
+               _bullToken.burnFrom(msg.sender, SafeMath.div(_bullAllowance, ratio));
+            }
+            if (_bearAllowance > 0) {
+                _bearToken.burnFrom(msg.sender, SafeMath.div(_bearAllowance, ratio));
+            }
+        }
+        uint _protocolFee = SafeMath.mul(_amount, _market.protocolFee());
+
+        ERC20(_market.collateralToken()).transfer(msg.sender, SafeMath.sub(_amount, _protocolFee));
+        ERC20(_market.collateralToken()).transfer(factory, _protocolFee);
+
+        //Increase total redemed collateral
+        markets[_marketAddress].totalRedemption = SafeMath.add(markets[_marketAddress].totalRedemption, _amount);
+
+        //TODO: use the event
+        // emit Redeem(msg.sender, _amount, now);
+    }
+
     function isMarket(address _market) public view returns (bool) {
-        return markets[_market];
+        return markets[_market].exist;
     }
 
     function setProtocolFee(uint _protocolFee)
@@ -198,7 +327,7 @@ contract MarketFactory is Ownable, ChainlinkData {
             // Clear the address of the pair
             colleteralCurrencies[_currencyName] = address(0);
         }
-        emit SetCurrency(_currencyName, _currencyToken, now);
+        // emit SetCurrency(_currencyName, _currencyToken, now);
     }
 
     function cloneMarket(
@@ -218,9 +347,9 @@ contract MarketFactory is Ownable, ChainlinkData {
         Market _market = Market(Clones.clone(baseMarket));
         // emit NewMarket(address(_market), now);
         _market.cloneConstructor(
-            _collateralToken,
-            _bullToken,
-            _bearToken,
+            address(_collateralToken),
+            address(_bullToken),
+            address(_bearToken),
             _duration,
             _collateralCurrency,
             _feedCurrencyPair,
@@ -242,9 +371,6 @@ contract MarketFactory is Ownable, ChainlinkData {
     {
         //Mint bear and bull tokens
         _conditionalToken.mint(address(this), _conditionalBalance);
-
-        //To allow the market to mint a conditional token
-        _conditionalToken.transferOwnership(_market);
 
         addToken(_market, _conditionalToken, _conditionalBalance, CONDITIONAL_TOKEN_WEIGHT);
     }
