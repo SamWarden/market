ee08effbb09ba7184cdd7da34a9690aa4d56b14c
SamWarden <SamWardenSad@gmail.com>
2021-04-17 21:04:13 +0300

    Delete meaningless methods in the OwnableClone

--- contracts/OwnableClone.sol
+++ contracts/OwnableClone.sol
@@ -20,13 +20,6 @@ abstract contract OwnableClone is Context {
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    // constructor () internal {
-    //     cloneConstructor();
-    // }
-
     function cloneConstructor () internal virtual {
         address msgSender = _msgSender();
         _owner = msgSender;
@@ -46,18 +39,6 @@ abstract contract OwnableClone is Context {
         require(owner() == _msgSender(), "Ownable: caller is not the owner");
         _;
     }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    // function renounceOwnership() public virtual onlyOwner {
-    //     emit OwnershipTransferred(_owner, address(0));
-    //     _owner = address(0);
-    // }
 
     /**
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
