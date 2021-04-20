cccd5a33b9e91f0ed2c9b9ce6e5a54dd7722b52a
SamWarden <SamWardenSad@gmail.com>
2021-04-20 14:16:31 +0300

    Get cloned contract with the clone attribute in src/clones

--- src/clones.ts
+++ src/clones.ts
@@ -1,10 +1,9 @@
-import type { TransactionResponse, ContractFactory, TClonesContract, TransactionReceiptWithEvents } from "./types";
+import type { ContractFactory, TClonesContract } from "./types";
 
 
 export async function cloneContract(address: string, contractFactory: ContractFactory<any>, tclones: TClonesContract) {
-  const txResponse: TransactionResponse = await tclones.copy(address);
-  const txResponseWithEvent: TransactionReceiptWithEvents = await txResponse.wait();
-  const cloneAddress = txResponseWithEvent.events![0].args[0];
+  await tclones.copy(address);
+  const cloneAddress = await tclones.clone();
   const contract = contractFactory.attach(cloneAddress);
   return contract;
 }
