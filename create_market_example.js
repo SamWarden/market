5e022c4d32671fc9aa4b36b2f3234a7b39e4278d
SamWarden <SamWardenSad@gmail.com>
2021-04-20 16:06:12 +0300

    Fix the create_market_example

--- create_market_example.js
+++ create_market_example.js
@@ -1,35 +1,35 @@
 //Example creating of a Market. Paste it to `hardhat console` to execute
-{formatEther, parseEther} = ethers.utils
-MarketFactory = await ethers.getContractFactory("MarketFactory");
-TToken = await ethers.getContractFactory("TToken");
-Market = await ethers.getContractFactory("Market");
-ConditionalToken = await ethers.getContractFactory("ConditionalToken");
-a = await ethers.getSigner();
-dai = await TToken.deploy("Dai", "Dai", 18);
+let {formatEther, parseEther} = ethers.utils;
+let MarketFactory = await ethers.getContractFactory("MarketFactory");
+let TToken = await ethers.getContractFactory("TToken");
+let Market = await ethers.getContractFactory("Market");
+let ConditionalToken = await ethers.getContractFactory("ConditionalToken");
+let acc = await ethers.getSigner();
+let dai = await TToken.deploy("Dai", "Dai", 18);
 await dai.deployed();
-ct = await ConditionalToken.deploy();
+let ct = await ConditionalToken.deploy();
 await ct.deployed();
-link = await TToken.attach("0xa36085F69e2889c224210F603D836748e7dC0088");
+let link = await TToken.attach("0xa36085F69e2889c224210F603D836748e7dC0088"); //Kovan link-token
 await link.deployed();
-m = await Market.deploy();
-await m.deployed();
-mf = await MarketFactory.deploy(m.address, ct.address);
+let bm = await Market.deploy();
+await bm.deployed();
+let mf = await MarketFactory.deploy(bm.address, ct.address);
 await mf.deployed();
 await mf.setBaseCurrency("ETH", true);
 await mf.setCollateralCurrency("DAI", dai.address);
 await mf.setProtocolFee(parseEther("0.003"))
-await dai.mint(a.address, ethers.utils.parseEther("20000"));
-await dai.approve(mf.address, ethers.utils.parseEther("10000"));
-await link.approve(mf.address, ethers.utils.parseEther("0.2"));
-await mf.create("ETH", "DAI", 600, ethers.utils.parseEther("10000"));
-v = Market.attach(mf.marketList(0));
-await dai.approve(v.address, ethers.utils.parseEther("10000"));
-bull = ConditionalToken.attach(await v.bullToken());
-bear = ConditionalToken.attach(await v.bearToken());
+await dai.mint(acc.address, parseEther("20000"));
+await dai.approve(mf.address, parseEther("10000"));
+await link.approve(mf.address, parseEther("0.2"));
+await mf.create("ETH", "DAI", 600, parseEther("10000"));
+let m = Market.attach(mf.marketList(0));
+await dai.approve(m.address, parseEther("10000"));
+let bull = ConditionalToken.attach(await m.bullToken());
+let bear = ConditionalToken.attach(await m.bearToken());
 
-await v.buy(ethers.utils.parseEther("1000"));
-await bull.approve(v.address, ethers.utils.parseEther("100"));
-await bear.approve(v.address, ethers.utils.parseEther("100"));
+await m.buy(parseEther("1000"));
+await bull.approve(m.address, parseEther("100"));
+await bear.approve(m.address, parseEther("100"));
 
-await v.close();
-await v.redeem(parseEther("100"))
+await m.close();
+await m.redeem(parseEther("100"));
